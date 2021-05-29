4ed4da560db0e590e372d9d211dfe258e2cda489
Aidan Kirvan <apkirvan@hotmail.com>
2021-05-29 12:13:13 +1000

    nav bar and basiline css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import React, { Component } from 'react'
+import Nav from './components/Nav'
 
-function App() {
-  return (
+const App = () => {
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <h1>hello world</h1>
     </div>
-  );
+  )
 }
 
 export default App;
