53383ac408445805605164c99d8ab7c3bd535e93
Aidan Kirvan <apkirvan@hotmail.com>
2021-05-30 12:06:46 +1000

    tailwind now works. Aditionally, buttons now print the correct information without delay because i got carried away

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,46 @@
 import React, {Component} from 'react'
+import { render } from '@testing-library/react'
 
 class Nav extends Component {
     constructor(props) {
         super(props)
     }
 
-    render () {
+    handleClick = (event) => {
+        const input = event.target.value
+        console.log("You clicked", input)
+        this.sendData(input)
+    }
+
+    sendData = (input) => {
+        this.props.parentCallback(input)
+    }
+
+    render(){
         return (
             <header className="App-header">
-                <nav className="Nav-btn-container">
-                    <button>HOME</button>
-                    <button>PROJECTS</button>
-                    <button>ABOUT</button>
-                    <button>CONTACT</button>
+                {/* NAV TOP */}
+                <nav className="Nav-btn-container-top">
+                    {/* NAV LEFT */}
+                    <nav className="Nav-btn-container-left">
+                        <button value="home" onClick={this.handleClick} >HOME</button>
+                    </nav>
+                    
+                    {/* NAV MIDDLE  */}
+                    <nav className="Nav-btn-container-middle">
+                        <h1 className="font-bold">AIDAN KIRVAN</h1>
+                    </nav>
+                    {/* NAV BOTTOM */}
+                    <nav className="Nav-btn-container-right">
+                        <button>GIT</button>
+                        <button>TWI</button>
+                    </nav>
+                </nav>
+                {/* NAV BOTTOM */}
+                <nav className="Nav-btn-container-bottom">
+                    <button value="projects" onClick={this.handleClick} >PROJECTS</button>
+                    <button value="about" onClick={this.handleClick} >ABOUT</button>
+                    <button value="contact" onClick={this.handleClick} >CONTACT</button>
                 </nav>
             </header>
         )
