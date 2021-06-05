c192776db20d26c997ca54a56d1f947e3c04a80b
Aidan Kirvan <apkirvan@hotmail.com>
2021-06-05 15:31:23 +1000

    added content and styling to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,45 +2,105 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className="page-component-container-projects">
-            <h1 className="page-heading text-3xl text-left">ABOUT</h1>
-            <div>
-            <p>Here is the summary</p>
-            <img alt={'profile'} src={"https://avatars.githubusercontent.com/u/67266954?v=4"} />
+        <div className="page-component-container-about w-full p-5">
+            <h1 className="page-heading text-3xl text-left">ABOUT ME</h1>
+            <div className="profile-container
+                            flex
+                            items-center
+                            justify-center
+                            flex-col
+                            sm:flex-row
+                            w-full
+                            sm:w-11/12
+                            my-5">
+                <div className="profile-portrait-container flex justify-center items-center w-11/12 sm:w-4/12 xl:w-3/12">
+                    <img alt={'profile'} src={"https://avatars.githubusercontent.com/u/67266954?v=4"} className="profile-portrait" />
+                </div>
+                
+                <div className="profile-info mt-5 sm:mt-0 w-full sm:w-8/12 xl:w-9/12 h-3/6 flex flex-col items-center justify-center">
+                    <h1 className="summary-table-head w-10/12 xl:w-8/12">Full Stack Developer</h1>
+                    <table className="summary-table w-11/12 xl:w-4/6">
+                        <tbody>
+                            <tr>
+                                <td>Artist</td>
+                                <td>Musician</td>
+                            </tr>
+                            <tr>
+                                <td>Writer</td>
+                                <td>DM / GM</td>
+                            </tr>
+                            <tr>
+                                <td>Lizard Dad</td>
+                                <td>Cat Servant</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
-            
-            <h1 className="page-heading text-3xl text-left">CORE SKILLS</h1>
-            <p>Here are my core skills</p>
-            <ul>
-                <li>Leadership</li>
-                <li>Communication</li>
-                <li>Problem Solving</li>
-                <li>Customer Service</li>
-                <li>Teaching</li>
-                <li>Conflict Resolution</li>
-                <li>Learning</li>
-                <li>Time Management</li>
-            </ul>
+
+            {/* OTHER SKILLS  */}
             <h1 className="page-heading text-3xl text-left">OTHER SKILLS</h1>
-            <p>Here are my other skils</p>
-            <ul>
-                <li>Illustration</li>
-                <li>Writing</li>
-                <li>Logic X</li>
-                <li>Ableton Live</li>
-                <li>Procreate</li>
-                <li>Guitar/Saxophone/Keys</li>
-            </ul>
-            <h1 className="page-heading text-3xl text-left">COMMUNITY AND OTHER INTERESTS</h1>
-            <p>Here are things that I am interested in</p>
-            <ul className="text-left">
-                <li>Tabletop Gaming</li>
-                <li>Roleplay Gaming</li>
-                <li>Videogames</li>
-                <li>Web and App integration for Tabletop Gaming</li>
-                <li>Music</li>
-                <li>Sound Recording and Production</li>
-            </ul>
+            <table className="about-table w-11/12 xl:w-4/6">
+                <tbody>
+                    <tr>
+                        <td>Illustration</td>
+                        <td>Writing</td>
+                    </tr>
+
+                    <tr>
+                        <td>Sound Engineering</td>
+                        <td>Images/Graphics</td>
+                    </tr>
+
+                    <tr>
+                        <td>Logic X</td>
+                        <td>Ableton Live</td>
+                    </tr>
+
+                    <tr>
+                        <td>Procreate</td>
+                        <td>Photoshop</td>
+                    </tr>
+
+                    <tr>
+                        <td>Saxophone</td>
+                        <td>Guitar</td>
+                    </tr>
+                </tbody>
+
+            </table>
+
+            {/* INTERESTS  */}
+            <h1 className="page-heading text-3xl text-left">INTERESTS</h1>
+            <table className="about-table w-11/12 xl:w-4/6">
+                <tbody>
+                    <tr>
+                        <td>Tabletop Gaming</td>
+
+                    </tr>
+
+                    <tr>
+                        <td>Roleplay Gaming</td>
+                    </tr>
+
+                    <tr>
+                        <td>Web and App Integration for Tabletop Gaming</td>
+                    </tr>
+
+                    <tr>
+                        <td>Videogames</td>
+                    </tr>
+
+                    <tr>
+                        <td>Music</td>
+                    </tr>
+
+                    <tr>
+                        <td>Sound Recording and Production</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
