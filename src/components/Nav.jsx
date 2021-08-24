cbda27f53edc1827b6c2a6ac2a8c38967afb2f4b
Aidan Kirvan <apkirvan@hotmail.com>
2021-08-24 20:14:16 +1000

    added routing in place of callback function

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,22 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import {
+    Link
+} from "react-router-dom"
 
 class Nav extends Component {
-
-    handleClick = (event) => {
-        console.log("event:",event.target.value)
-        const input = event.target.value
-        console.log("You clicked", input)
-        this.sendData(input)
-    }
-
-    sendData = (input) => {
-        this.props.parentCallback(input)
-    }
-
-    render(){
+    render() {
         return (
             <header className="App-header">
                 {/* NAV TOP */}
                 <nav className="Nav-btn-container-top">
-                    {/* NAV LEFT */}
+                    {/* NAV TOP LEFT */}
                     <nav className="Nav-btn-container-left">
-                        <button value="home" onClick={this.handleClick} className="heading mx-5 px-2 flex flex-row">
+                        <Link to="/" className="heading mx-5 px-2 flex flex-row">
                             <span className="heading-a font-bold text-xl px-2 sm:text-4xl mx-2">Aidan</span>
                             <span className="heading-b font-bold text-xl sm:text-4xl">KIRVAN</span>
-                        </button>
+                        </Link>
                     </nav>
-                    {/* NAV BOTTOM */}
+                    {/* NAV TOP RIGHT */}
                     <nav className="Nav-btn-container-right">
                         <a href="https://github.com/teraglin" target="_blank" rel="noopener noreferrer">
                             <img className="svg-icon w-10 transform hover:scale-110 transition delay-50 ease-in-out" src={require('../images/github-icon.svg').default} alt='github-link' />
@@ -38,9 +29,17 @@ class Nav extends Component {
                 </nav>
                 {/* NAV BOTTOM */}
                 <nav className="Nav-btn-container-bottom">
-                    <button value="projects" onClick={this.handleClick} className="px-2" >PROJECTS</button>
-                    <button value="about" onClick={this.handleClick} className="px-2" >ABOUT</button>
-                    <button value="contact" onClick={this.handleClick} className="px-2" >CONTACT</button>
+                    {/* PROJECTS */}
+                    <button className="px-2">
+                        <Link to="projects">PROJECTS</Link>
+                    </button>
+                    {/* ABOUT */}
+                    <button className="px-2">
+                        <Link to="about">ABOUT</Link>
+                    </button>
+                    <button className="px-2">
+                        <Link to="contact">CONTACT</Link>
+                    </button>
                 </nav>
             </header>
         )
