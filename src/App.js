c6aa6bd97af711b4524990c60f58f62896b9a3f5
Aidan Kirvan <apkirvan@hotmail.com>
2024-05-03 16:01:33 +1000

    Feature: react router installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './styles/App.css';
-import React from 'react';
+import "./styles/App.css";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { MainPage } from './pages/MainPage';
+import { MainPage } from "./pages/MainPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />
+  },
+  {
+    path: "/*",
+    element: <h1 style={{ color: "white" }}>404</h1>
+  }
+]);
 
 const App = () => {
-  return (
-    <MainPage />
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
